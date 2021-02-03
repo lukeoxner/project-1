372ede81c304c6e109fbbff7d67e1279f517fedb
Drew Weston <awfiscus@comcast.net>
2021-02-03 14:07:17 -0700

    jokes now show up on availble divs, and category buttons are added to an array

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 
 
+var category = []
+
 $("#submitBtn").on("click", function() {
 
+var formatCategory = category.join(",")
+console.log(formatCategory);
 
-var category = "Misc,Misc"
-var queryURL = "https://v2.jokeapi.dev/joke/" + category + "?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
+var queryURL = "https://v2.jokeapi.dev/joke/" + formatCategory + "?blacklistFlags=nsfw,religious,political,racist,sexist,explicit"
 
 $.ajax({
 	url: queryURL,
@@ -23,10 +26,10 @@ $.ajax({
 
 	//add if statement to determine if it is a two part joke or a one part
 	if(jokeType === "twopart") {
-		console.log(setup);
-		console.log(delivery);
+		$("#joke-result").text(setup)
+		$("#punchline-result").text(delivery)
 	} else {
-		console.log(joke);
+		$("#joke-result").text(joke)
 	}
 
 	//add functionality that displays data from response to matching DOM elements
@@ -39,4 +42,9 @@ $.ajax({
 })
 
 //build a function that determines which catergories have been selected and put them into an array
+
+$(".category").on("click", function() {
+	category.push(this.textContent)
+	console.log(this.textContent);
+})
 
